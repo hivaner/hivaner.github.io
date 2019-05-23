3ae8a09c40e7f121054c4a5c7de43d574431cd98
hivanerd <hivan.travel@gmail.com>
2019-05-23 23:35:31 +0800

    header和footer抽离为公用代码

---
+++ layout_header.js
@@ -0,0 +1,22 @@
+document.write(`
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+<div class="container">
+  <a class="navbar-brand" href="index.html"><h6>微信 Neva889900<br>电话 (+63) 09174094367</h6></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="oi oi-menu"></span> Menu
+  </button>
+
+  <div class="collapse navbar-collapse" id="ftco-nav">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active"><a href="index.html" class="nav-link">首页</a></li>
+      <li class="nav-item"><a href="about.html" class="nav-link">关于</a></li>
+      <li class="nav-item"><a href="tour.html" class="nav-link">一日游</a></li>
+      <li class="nav-item"><a href="hotel.html" class="nav-link">度假游</a></li>
+      <li class="nav-item"><a href="blog.html" class="nav-link">旅游目的地</a></li>
+      <li class="nav-item"><a href="contact.html" class="nav-link">联系我们</a></li>
+      <li class="nav-item cta"><a href="contact.html" class="nav-link"><span>在线客服</span></a></li>
+    </ul>
+  </div>
+</div>
+</nav>
+`)
