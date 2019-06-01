4328477f200a841d3f4159db6f2e5709096c7c3a
hivanerd <hivan.travel@gmail.com>
2019-06-01 09:02:27 +0800

    加入谷歌在线预订

--- data/layout_header.js
+++ data/layout_header.js
@@ -14,7 +14,7 @@ document.write(`
       <li class="nav-item"><a href="hotel.html" class="nav-link">度假游</a></li>
       <li class="nav-item"><a href="blog.html" class="nav-link">旅游目的地</a></li>
       <li class="nav-item"><a href="contact.html" class="nav-link">联系客服</a></li>
-      <li class="nav-item cta"><a href="contact.html" class="nav-link"><span>预订</span></a></li>
+      <li class="nav-item cta"><a href="https://docs.google.com/forms/d/e/1FAIpQLSeDQQTC095RgBhI3VkjdcdFCY-G1F7GUtItxhyWZ-daYOd7jQ/viewform?entry.672735455=%E9%BB%91%E6%B5%B7%E6%BB%A9%E4%B8%80%E6%97%A5%E6%B8%B8&entry.1509848516=2019-06-01&entry.1072873268=v&entry.554966761=32" class="nav-link"><span>预订</span></a></li>
     </ul>
   </div>
 </div>
