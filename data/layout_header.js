262c68fab458805a7698ea839f044e688c87aaba
hivanerd <hivan.travel@gmail.com>
2019-06-02 22:55:19 +0800

    在线客服自动常见问题FAQ咨询

--- data/layout_header.js
+++ data/layout_header.js
@@ -1,20 +1,20 @@
 document.write(`
 <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
 <div class="container">
-  <a class="navbar-brand" href="index.html"><h6>微信 Neva889900<br>电话 (+63) 09174094367</h6></a>
+  <a class="navbar-brand" href="${contact.wechatUrl}"><h6>微信咨询 ${contact.wechat}<br>电话咨询 ${contact.mobile}</h6></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="oi oi-menu"></span> Menu
+    <span class="oi oi-menu"></span> 菲律宾一日游
   </button>
 
   <div class="collapse navbar-collapse" id="ftco-nav">
     <ul class="navbar-nav ml-auto">
       <li class="nav-item active"><a href="index.html" class="nav-link">首页</a></li>
-      <li class="nav-item"><a href="about.html" class="nav-link">关于</a></li>
-      <li class="nav-item"><a href="tour.html" class="nav-link">一日游</a></li>
-      <li class="nav-item"><a href="hotel.html" class="nav-link">度假游</a></li>
-      <li class="nav-item"><a href="blog.html" class="nav-link">旅游目的地</a></li>
+      <li class="nav-item"><a href="tour.html" class="nav-link">特惠一日游</a></li>
+      <li class="nav-item"><a href="hotel.html" class="nav-link">休闲一日游</a></li>
+      <li class="nav-item"><a href="blog.html" class="nav-link">精品一日游</a></li>
       <li class="nav-item"><a href="contact.html" class="nav-link">联系客服</a></li>
-      <li class="nav-item cta"><a href="https://docs.google.com/forms/d/e/1FAIpQLSeDQQTC095RgBhI3VkjdcdFCY-G1F7GUtItxhyWZ-daYOd7jQ/viewform?entry.672735455=%E9%BB%91%E6%B5%B7%E6%BB%A9%E4%B8%80%E6%97%A5%E6%B8%B8&entry.1509848516=2019-06-01&entry.1072873268=v&entry.554966761=32" class="nav-link"><span>预订</span></a></li>
+      <li class="nav-item"><a href="about.html" class="nav-link">常见问题</a></li>
+      <li class="nav-item cta"><a href="${contact.bookUrl}" class="nav-link"><span>立即预订</span></a></li>
     </ul>
   </div>
 </div>
